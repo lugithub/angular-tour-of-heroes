1fa2d8ac8261c50e92836ff282a679aff13ad447
lubin <lu.bin.lu.bin@gmail.com>
2017-11-12 00:55:42 -0500

    step 7: routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -17,11 +17,5 @@ export class HeroesComponent implements OnInit {
         .subscribe(heroes => this.heroes = heroes);
   }
 
-  selectedHero: Hero;
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
   heroes: Hero[];
 }
